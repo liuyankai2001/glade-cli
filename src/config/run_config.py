3a95f4b89408026cd16dcdc7162fceba1fb4dbee
liuyankai <liuyankai23@mails.ucas.ac.cn>
2026-08-14 16:43:32 +0800

    分析底盘细胞可提供代谢物完成

--- src/config/run_config.py
+++ src/config/run_config.py
@@ -4,26 +4,25 @@ from pathlib import Path
 from dataclasses import dataclass
 from src.config import config
 
-@dataclass
 class RunConfig:
-    # 基础配置
-    root_dir:Path = config.ROOT
-    data_dir:Path = config.DATA_DIR
-    gem_dir:Path = config.GEM_DIR
-    medium_dir:Path = config.MEDIUM_DIR
-    inputs_dir:Path = config.INPUTS_DIR
-    outputs_dir:Path = config.OUTPUTS_DIR
-    cache_dir:Path = config.CACHE_DIR
+    def __init__(self, target_name: str = None):
+        # 基础配置
+        self.root_dir: Path = config.ROOT
+        self.data_dir: Path = config.DATA_DIR
+        self.gem_dir: Path = config.GEM_DIR
+        self.medium_dir: Path = config.MEDIUM_DIR
+        self.inputs_dir: Path = config.INPUTS_DIR
+        self.outputs_dir: Path = config.OUTPUTS_DIR
+        self.cache_dir: Path = config.CACHE_DIR
 
-    model_path:Path = gem_dir / 'iML1515.json'
-    medium_path:Path = medium_dir / 'default_medium.json'
+        self.model_path: Path = self.gem_dir / 'iML1515.json'
+        self.medium_path: Path = self.medium_dir / 'default_medium.json'
 
-    target_name:str = None
-    project_output_path:Path = outputs_dir / target_name
+        self.target_name: str = target_name
+        self.project_output_path: Path = self.outputs_dir / self.target_name
 
-    # 底盘细胞分析输出文件
-    chassis_output_path:Path = project_output_path / 'chassis_result'
-    chassis_producible_csv:Path = chassis_output_path / 'producible_kegg_compounds.csv'
-    chassis_metabolites_summary_csv:Path = chassis_output_path / 'analyze_chassis_metabolites_summary.csv'
+        # 底盘细胞分析输出文件
+        self.chassis_output_path: Path = self.project_output_path / 'chassis_result'
+        self.chassis_producible_csv: Path = self.chassis_output_path / 'producible_kegg_compounds.csv'
+        self.chassis_metabolites_summary_csv: Path = self.chassis_output_path / 'analyze_chassis_metabolites_summary.csv'
 
-    
